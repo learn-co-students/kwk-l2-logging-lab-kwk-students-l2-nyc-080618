6dd1962781361488df24adb7fddecbe110e0658a
Pooja  Tetali <pooja.tetali@gmail.com>
2018-08-08 16:07:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const driver = 'bob';
+var ThisCodeWasCalled = "this code was called"; 
+console.log(ThisCodeWasCalled); 
+console.log(driver); 
+console.log(driver, "is the driver variable name"); 
